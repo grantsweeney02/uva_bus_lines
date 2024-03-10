ed96efec469532c1979dbb9554d97d9577a78ccd
Grant Sweeney <grantsweeney02@gmail.com>
2024-03-10 16:02:25 -0400

    Initial commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,27 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
+    GeneratedPluginRegistrant.register(with: self)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "com.example.uva_bus_lines/channel",
+                                       binaryMessenger: controller.binaryMessenger)
+
+    channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        if call.method == "setMapsApiKey" {
+            if let key = call.arguments as? String {
+                GMSServices.provideAPIKey(key)
+            }
+        }
+    })
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
